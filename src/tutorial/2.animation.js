a119ef2eeb41201294825d0533b0a84696a4317c
Andrew Heekin <andrewheekin@gmail.com>
2018-05-06 20:42:12 -0400

    reverse ball direction if it hits side

--- src/tutorial/2.animation.js
+++ src/tutorial/2.animation.js
@@ -5,9 +5,11 @@ export const start = (ctx, canvas) => {
   let dx = 2;
   let dy = -2;
 
+  const ballRadius = 10
+
   const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
@@ -18,6 +20,10 @@ export const start = (ctx, canvas) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     drawBall();
+
+    // reverse ball X or Y direction if side is reached
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) dx = -dx;
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) dy = -dy;
 
     // add the motion to the x and y coords
     x += dx;
